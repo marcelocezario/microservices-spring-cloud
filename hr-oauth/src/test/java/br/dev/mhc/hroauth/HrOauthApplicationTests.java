a0f6a61239ad00c4e8a09a4b0316f1f540ca9f69
Marcelo Cezário <marcelocezario@gmail.com>
2021-10-22 20:21:10 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/test/java/br/dev/mhc/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.mhc.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
