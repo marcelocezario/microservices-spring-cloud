d84d4c715a14a53830862309d9bacec630103ce6
Marcelo Cezário <marcelocezario@gmail.com>
2021-10-21 22:43:18 -0300

    Project hr-api-gatway-zuul created

---
+++ hr-api-gateway-zuul/src/test/java/br/dev/mhc/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.mhc.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
